554fc0fe7d5082084f9a3e7a4e47784405917fba
Aleksandra Okrój <aleksandra.okroj1@gmail.com>
2021-06-06 11:27:44 +0200

    API /get connected

---
+++ src/components/AddPost.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+        <br>
+        <br><br>
+        <form @submit="addPost">
+        <div class="form-group">
+            <input required type="text" v-model="title" placeholder="treść" name="title" id="" class="form-control">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-block btn-danger">
+                Dodaj
+            </button>
+        </div>
+        </form>
+    </div>
+</template>
+<script>
+
+export default {
+  name: "AddPost",
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addPost(e) {
+        e.preventDefault()
+      const newPost = {
+        id:Date.now(),
+        title: this.title,
+      }
+      // Send up to parent
+      this.$emit('add-post', newPost);
+      this.title = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
